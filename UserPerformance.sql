6175ffb10b4475a6bf7baff69ac1625a8b7df053
shukisoft <mmmshukri@gmail.com>
2023-06-10 15:28:13 +0530

    edited for duplicate

--- UserPerformance.sql
+++ UserPerformance.sql
@@ -2,3 +2,7 @@ select ModifiedUser ,count(LoyaltyCustomerID) as TotalApps from LoyaltyCustomer 
 and CreatedDate<>ModifiedDate
 group by ModifiedUser order by TotalApps desc
 
+select ModifiedUser ,count(LoyaltyCustomerID) as TotalApps from LoyaltyCustomer where cast(ModifiedDate as date)=cast(getdate()-1 as date)
+and CreatedDate<>ModifiedDate
+group by ModifiedUser order by TotalApps desc
+
